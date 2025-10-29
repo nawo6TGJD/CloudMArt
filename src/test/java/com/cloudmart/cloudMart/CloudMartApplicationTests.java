11eac91b55558b0b82e49d1949616d65716f9c4d
nawo6TGJD <129091686+nawo6TGJD@users.noreply.github.com>
2025-10-29 19:43:32 +0530

    Initial commit

---
+++ src/test/java/com/cloudmart/cloudMart/CloudMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudmart.cloudMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
